e62fcd43785ac066cdf44808021c5d84e5e8c820
Pablo <7073164+dsapab@users.noreply.github.com>
2025-08-10 03:52:53 +0100

    Fixing EFS, for real, again.

--- terraform/storage/main.tf
+++ terraform/storage/main.tf
@@ -76,9 +76,7 @@ resource "aws_efs_file_system_policy" "main" {
     Statement = [
       {
         Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
+        Principal = "*"
         Action = "*"
         Resource = aws_efs_file_system.main.arn
       }
