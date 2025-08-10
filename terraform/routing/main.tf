22c4d2ecf94d75df0162ffbd61175b8360e947f9
Pablo <7073164+dsapab@users.noreply.github.com>
2025-08-10 17:23:19 +0100

    Fixing CF. Still needs testing.

--- terraform/routing/main.tf
+++ terraform/routing/main.tf
@@ -5,6 +5,7 @@
 ## -------------------------------------------------------------------------------------------------------------------
 data "aws_region" "current" {}
 
+
 ## -------------------------------------------------------------------------------------------------------------------
 ## CloudFront Origin Access Control
 ## -------------------------------------------------------------------------------------------------------------------
@@ -15,6 +16,26 @@ resource "aws_cloudfront_origin_access_control" "main" {
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
+
+
+## -------------------------------------------------------------------------------------------------------------------
+## VPC Origin
+## -------------------------------------------------------------------------------------------------------------------
+resource "aws_cloudfront_vpc_origin" "cf_origin" {
+  vpc_origin_endpoint_config {
+    name                   = "${var.resources_prefix_name}-vpc-origin"
+    arn                    = "arn:aws:ec2:us-east-1:123456789012:instance/i-1234567890abcdef0"
+    http_port              = 80
+    https_port             = 443
+    origin_protocol_policy = "http-only"
+
+    origin_ssl_protocols {
+      items    = ["TLSv1.2"]
+      quantity = 1
+    }
+  }
+}
+
 
 ## -------------------------------------------------------------------------------------------------------------------
 ## CloudFront Distribution
@@ -29,40 +50,20 @@ resource "aws_cloudfront_distribution" "main" {
   http_version        = "http2and3"
   price_class         = "PriceClass_100"
 
-  ## 
-  # ORIGIN Config
-  create_vpc_origin = true
-  vpc_origin = {
-    ec2_vpc_origin = {
-      name                   = random_pet.this.id
-      arn                    = module.ec2.arn
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = {
-        items    = ["TLSv1.2"]
-        quantity = 1
-      }
-    }
-  }
-
   # Default placeholder origin - will be updated by Lambda
   origin {
-    domain_name = "example.com"
+    domain_name = "placeholder-for-private-instance-ip"
     origin_id   = "${var.resources_prefix_name}-cf-origin"
 
-    custom_origin_config {
-      http_port              = var.container_port
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
+    vpc_origin_config {
+      vpc_origin_id = aws_cloudfront_vpc_origin.cf_origin.id
     }
   }
 
   default_cache_behavior {
     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "placeholder"
+    target_origin_id       = aws_cloudfront_vpc_origin.cf_origin.id
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
 
